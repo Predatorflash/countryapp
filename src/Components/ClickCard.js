90c8018acd8a7998404658f392d48b1689f571f4
Srivatsa Rao <PredatorFlash>
2018-10-25 21:41:02 +0530

    stage3

--- src/Components/ClickCard.js
+++ src/Components/ClickCard.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { countryjson } from "./countryjson";
+import Showinfocard from "./Showinfocard";
+
 class ClickCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      cardstate: ""
+    };
   }
   componentDidMount = () => {
+    let a = 10,
+      b = 15;
+    const mycode = () => {
+      const countrycodecalc = countryjson.filter(value => {
+        if (value.name === this.props.countryname) return true;
+      });
+      console.log("i  am in mycode", countryjson, countrycodecalc);
+      return countrycodecalc[0].code;
+    };
+
     axios
-      .get(`http://restcountries.eu/rest/v2/alpha/${this.countrycode}`)
-      .then(res => console.log(res))
+      .get(`http://restcountries.eu/rest/v2/alpha/${mycode()}`)
+      .then(res => {
+        console.log("I am inside axios", res);
+        this.setState({
+          cardstate: res
+        });
+      })
       .catch(err => console.log(err));
   };
 
   render() {
-    const countrycode = () => {
-      "IN";
-    };
     return (
-      <h1>
-        {this.props.countryname}
-        {console.log(countrycode)}
-      </h1>
+      <div>
+        {" "}
+        <h1>{this.props.countryname}</h1>
+        <Showinfocard detail={this.state.cardstate} />
+      </div>
     );
   }
 }
